a8c2b59845047c86a230a494750db5e661acd3c8
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-27 06:25:32 +0500

    Implement result pattern

---
+++ Shared/Error.cs
@@ -0,0 +1,6 @@
+﻿namespace TestingProject.Shared;
+
+public record Error(string Name, string Description)
+{
+    public static Error NullValue => new("NullValue", "Value was null");
+}
---
+++ Shared/Result.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+namespace TestingProject.Shared;
+
+public class Result
+{
+    private readonly Error? _error = null;
+    private readonly bool _isSuccess;
+
+    protected internal Result()
+    {
+        _isSuccess = true;
+    }
+
+    protected internal Result(Error error)
+    {
+        _error = error;
+        _isSuccess = false;
+    }
+
+    public bool IsSuccess => _isSuccess;
+
+    public bool IsFailed => !IsSuccess;
+
+    [NotNull]
+    public Error Error => IsFailed ?
+        _error! :
+        throw new InvalidOperationException("Trying get error when result success");
+
+    public static Result Success() => new Result();
+
+    public static Result Failure(Error error) => new Result(error);
+
+    public static Result<TValue> Success<TValue>(TValue value) => new Result<TValue>(value);
+
+    public static Result<TValue> Failure<TValue>(Error error) => new Result<TValue>(error);
+
+    public static Result<TValue> Create<TValue>(TValue? value) =>
+        value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
+}
+
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value = default;
+
+    protected internal Result(TValue value) : base()
+    {
+        _value = value;
+    }
+
+    protected internal Result(Error error) : base(error)
+    {
+    }
+
+    [NotNull]
+    public TValue Value => IsSuccess ?
+        _value! :
+        throw new InvalidOperationException("Trying get value when result success");
+
+    public static implicit operator Result<TValue>(TValue? result) => Create(result);
+}
