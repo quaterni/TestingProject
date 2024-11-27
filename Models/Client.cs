6f0846eca5895d5e68a8182e6ffd04ada38c972c
Igor Zhadko <igorzhadko.w@gmail.com>
2024-11-27 06:14:54 +0500

    Override Equals and GetHashCode on Client

--- Models/Client.cs
+++ Models/Client.cs
@@ -8,4 +8,14 @@ public class Client
     public required string Username { get; set; }
 
     public required Guid SystemId { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Client client && client.ClientId.Equals(ClientId);
+    }
+
+    public override int GetHashCode()
+    {
+        return ClientId.GetHashCode();
+    }
 }
